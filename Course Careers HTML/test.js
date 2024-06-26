87537eb952712463e6e99ef3e1fd0a8657f21619
cherbon99 <77909512+cherbon99@users.noreply.github.com>
2024-06-25 23:14:41 -0600

    quick fix and commenting code

--- Course Careers HTML/test.js
+++ Course Careers HTML/test.js
@@ -2,4 +2,7 @@ console.log("hi earth");
 console.warn("warning live without warning")
 console.error("caution police line you better not cross")
 
-console.log(document.getElementById("name").innerHTML);
+//console.log(document.getElementById("name").innerHTML);
+
+var myNumber = 1;
+console.log(x);
