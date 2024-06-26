00a8c0e57f8ad9eb8ccdbf53926db89ada21c1f6
cherbon99 <77909512+cherbon99@users.noreply.github.com>
2024-06-25 22:54:30 -0600

    setting up javascript

---
+++ Course Careers HTML/test.js
@@ -0,0 +1,5 @@
+console.log("hi earth");
+console.warn("warning live without warning")
+console.error("caution police line you better not cross")
+
+console.log(document.getElementById("name").innerHTML);
