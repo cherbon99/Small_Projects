d68337695a5710ddcc4a2910d886e5a26722aa19
cherbon99 <77909512+cherbon99@users.noreply.github.com>
2024-06-27 23:51:50 -0600

    turning arrays into sets and sets into arrays

--- Course Careers HTML/test.js
+++ Course Careers HTML/test.js
@@ -1,11 +1,13 @@
 const arr = [1,1,1,2,3,5,8,9,4,5];
-for(const [idx, value] of arr.entries()){
-    for (const value2 of arr.slice(idx + 1)){
-        if( value === value2){
-            console.log("gotcha ya filthy duplicate", value);
-            break
-        }
-    }
+const numbers = new Set();
+
+for(const value of arr){
+   if (numbers.has(value)){
+    console.log("gotcha ya filthy duplicate", value)
+   }
+   numbers.add(value);
 }
+
+
 
 
