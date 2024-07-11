c3cbfa503ec5f9ac398ecc26b4b1b763b3ba921d
cherbon99 <77909512+cherbon99@users.noreply.github.com>
2024-07-10 22:38:42 -0600

    experimenting with random

--- Course Careers HTML/test.js
+++ Course Careers HTML/test.js
@@ -1,11 +1,36 @@
-const person = {
-   name: "chris",
-   lastName: "Herbon",
-   getName: function() {
-      console.log(this.name + this.lastName)
-   }
+function randomNum(min, max){
+   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function test() {
-   console.log("test");
+let num = randomNum(1, 100);
+let numGuesses = 0;
+
+function reset(){
+   num = randomNum(1, 100);
+   numGuesses = 0;
+   document.getElementById("num-guesses").innerHTML = 0;
+
+}
+   
+function makeGuess() {
+   const guessInput = document.getElementById("guess")
+   const guess = Number(document.getElementById("guess").value);
+   guessInput.value = "";
+   const feedback = document.getElementById("feedback")
+
+   numGuesses++;
+   document.getElementById("num-guesses").innerHTML = numGuesses;
+
+
+   if (guess === num) {
+      feedback.innerHTML = "you guessed it"
+      reset();
+   } else if(guess > num){
+      feedback.innerHTML = "its lower babe"
+   }else if(guess < num){
+      feedback.innerHTML = "its higher boi"
+   } else {
+      feedback.innerHTML = "better luck next time";
+
+   }
 }
