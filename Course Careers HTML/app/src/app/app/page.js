c15d6317b0d8676160dc512de4b1cecbbe544fed
cherbon99 <77909512+cherbon99@users.noreply.github.com>
2024-07-22 23:24:17 -0600

    starting react

---
+++ Course Careers HTML/app/src/app/app/page.js
@@ -0,0 +1,14 @@
+import { Post } from '@/components/Post';
+
+export default function Home() {
+  return ( 
+  <div>
+    <Post 
+    title="Speed tours" 
+    author="chris" 
+    content="THIS IS THE GARAGE"
+    />
+  </div>
+  );
+
+}
