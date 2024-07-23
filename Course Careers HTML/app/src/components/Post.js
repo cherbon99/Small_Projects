a050f0cd676c0cfeb6ed5f0c89a5bcc402718d67
cherbon99 <77909512+cherbon99@users.noreply.github.com>
2024-07-22 23:39:11 -0600

    fixing file structure

--- Course Careers HTML/app/src/components/Post.js
+++ Course Careers HTML/app/src/components/Post.js
@@ -1,8 +1,10 @@
-export default function Post({title, author, content }) {
+export function Post({title, author, content, following }) {
+
     return (
     <div>
         <h1>{title}</h1>
         <h3> By: {author}</h3>
+        <button>{ following ? "Unfollow" : "Follow"}</button>
         <p>{content}</p>
     </div>
     );
