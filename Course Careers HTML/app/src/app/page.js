b501ad6b3a1b011458e03d530caf1167d605569a
cherbon99 <77909512+cherbon99@users.noreply.github.com>
2024-07-23 22:40:59 -0600

    state practice

--- Course Careers HTML/app/src/app/page.js
+++ Course Careers HTML/app/src/app/page.js
@@ -3,18 +3,52 @@ import { Post } from "@/components/Post";
 import { useState } from "react";
 
 export default function Home() {
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState([]);
 
-  const [count, setCount] = useState(0);
-
-  function onCount(){
-    setCount(count +1);
+  function addPost() {
+    const newPost = {
+      author: author,
+      content: content,
+      title: title,
+      following: false,
+    };
+    setPosts([...posts, newPost]);
+    setContent("");
+    setAuthor("");
+    setTitle("");
   }
-
 
   return (
     <>
-      <p>{count}</p>
-      <button onClick={onCount}>Count Up!</button>
+      <div>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+        />
+        <input
+          type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Content"
+        />
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          placeholder="Author"
+        />
+        <button onClick={addPost}>Add Post</button>
+      </div>
+      <div className="posts">
+        {posts.map((post, idx) => (
+          <Post {...post} key={idx + Math.random()} />
+        ))}
+      </div>
     </>
   );
 }
