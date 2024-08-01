bca78b7b187ddbdb793a04afdfd7ddea8fa0558d
cherbon99 <77909512+cherbon99@users.noreply.github.com>
2024-07-31 22:20:18 -0600

    adding moves list to bottom of board

--- Course Careers HTML/app/src/app/TicTacToe/page.js
+++ Course Careers HTML/app/src/app/TicTacToe/page.js
@@ -75,11 +75,37 @@ export default function Game() {
   const xIsNext = currentMove % 2 == 0;
   const currentSquares = history[currentMove];
 
-  function handlePlay(nextSquares){}
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0){
+      description = "Go to move # " + move;
+    } else{
+      description = "Go to game start";
+    }
+    return <li key={move + Math.random()}>
+      <button onClick={() => jumpTo(move)}>{description}</button>
+    </li>
+  })
 
   return (
-    <div>
-      <Board squares={currentSquares} xIsNext={ xIsNext } onPlay={handlePlay} />
+    <div className="game">
+      <div className="game-board">
+        <Board squares={currentSquares} xIsNext={ xIsNext } onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
     </div>
   );
 }
