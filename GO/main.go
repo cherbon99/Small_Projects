2fe31b41bb7f89ad941c0df377c99eae33213758
cherbon99 <77909512+cherbon99@users.noreply.github.com>
2024-09-24 20:51:12 -0600

    implimenting API

--- GO/main.go
+++ GO/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/ginhttp"
+)
+
+type book struct{
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+var books = []book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2}
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5}
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6}
+}
+
+func getBooks(c *gin.Context) {
+	c.IndetedJSON(http.StatusOK, books)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run("localhost:8080")
+}
